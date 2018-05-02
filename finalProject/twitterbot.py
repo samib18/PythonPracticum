99b7a9f86009fbac17fe208335273f864738e59c
samib18 <sami.bialozynski@gmail.com>
2018-05-02 12:45:58 -0400

    Final Project documentation

--- finalProject/twitterbot.py
+++ finalProject/twitterbot.py
@@ -1,3 +1,7 @@
+#Chat Twitter Bots
+#I have one half of a chate twitter bot that will go through a conversation of things
+#The code checks to make sure that statuses are not repeated because using the tweepy library you cannot duplicate statuses
+#If there is a duplication error the bot will tweet about the current trends in Baltimore, MD area
 import tweepy
 import time
 from codes import * 
@@ -7,12 +11,13 @@ lat = float(39.2189)
 long = float(-76.0690)
 
 
-
+#The codes given from my specific twitter account
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
 auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
 
+#Getting the trends for Baltimore, MD
 trends = api.trends_place(2358820)
 data = trends[0]
 getTrends = data['trends']
@@ -20,6 +25,7 @@ names = [trend['name'] for trend in getTrends]
 
 allTrends = []
 
+#Placing the trends in a list so they can be tweeted
 for x in names: 
     allTrends.append(x)
  
@@ -33,25 +39,29 @@ while 1:
 #tweet = api.user_timeline(id = '983813133075656704', count = 1)[0]
     if notDuplicated == True:
         print("It's not duplicated")
+        #The id of the other Twitter bot user
         tweet = api.user_timeline(id = '983813133075656704', count = 1)[0]
         print(tweet.text)
+        #Set up for a message
         message = chatbot.get_response(tweet.text)
         
     else:
+        #If duplicated set message to a trend
         message = allTrends[count]
         count += 1
-        notDuplicated = True
-        
-    
+        notDuplicated = True    
    
     try:
+        #Set the status based on message
         api.update_status(message)
+        
     except tweepy.TweepError:
             print("Duplicated")
             notDuplicated = False
             
         
 
+    #Tweet every 40 seconds
     time.sleep(40)
     print("one loop end")
 
